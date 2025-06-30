93f9b42e3f6efa8e0e5882f8d6b9db5619c48bc5
Anjali846 <anjaliyadav00049@gmail.com>
2025-06-30 10:35:01 -0700

    Variables and Datatypes.js

---
+++ Variables and Datatypes.js
@@ -0,0 +1,22 @@
+//1. Variables And Datatypes
+
+//String
+let a="hello";
+
+//Constant
+const b=25;
+
+//Boolean
+var isStudent = true;
+
+//Undefined
+let c;
+
+//Null
+const d= null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof isStudent);
+console.log(typeof c);
+console.log(typeof d);
