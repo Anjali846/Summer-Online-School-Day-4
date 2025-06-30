f826ce9915f061cf304d49b6392178c0328ef159
Anjali846 <anjaliyadav00049@gmail.com>
2025-06-30 11:27:24 -0700

    loops.js

---
+++ loops.js
@@ -0,0 +1,20 @@
+//4.Loops
+
+//For Loop: Print Numbers from 1 to 10
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+    
+// While Loop: Print even numbers from 2 to 20
+let num=2;
+while(num<=20){
+    console.log(num);
+    num=num+2;
+}
+
+// Do While Loop: Print numbers from 10 down to 1
+let j=10;
+do{
+    console.log(j);
+    j--;
+}while(j>=1);
